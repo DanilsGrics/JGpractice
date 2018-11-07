8fa30384d98de861812a7d4656cfc9fe44cdc6d3
DanilsGrics <kamukaq3e@gmail.com>
2018-11-08 00:34:43 +0200

    minor editings

--- src/lv/danilsgrics/thirdLab/SignComparatorTest.java
+++ src/lv/danilsgrics/thirdLab/SignComparatorTest.java
@@ -3,35 +3,35 @@ package lv.danilsgrics.thirdLab;
 public class SignComparatorTest {
 
 
-            public static void main(String[] args) {
+    public static void main(String[] args) {
 
-                SignComparator comparator = new SignComparator();
-                int figure = 123;
+        SignComparator comparator = new SignComparator();
+        int figure = 123;
 
-                System.out.println(figure + ": " + comparator.compare(figure));
+        System.out.println(figure + ": " + comparator.compare(figure));
 
-                SignComparatorTest testRunner = new SignComparatorTest();
-                testRunner.isFigureNegativeTest();
-            }
+        SignComparatorTest testRunner = new SignComparatorTest();
+        testRunner.isFigureNegativeTest();
+    }
 
     public void isFigureNegativeTest() {
-            SignComparator victim = new SignComparator();
+        SignComparator victim = new SignComparator();
 
-            int testNegativeFigure = -21;
+        int testNegativeFigure = -21;
 
-            String expectedResult = "Number is negative!";
-            String actualResult = victim.compare(testNegativeFigure);
+        String expectedResult = "Number is negative!";
+        String actualResult = victim.compare(testNegativeFigure);
 
-            check(actualResult, expectedResult, "isFigureNegative");
+        check(actualResult, expectedResult, "isFigureNegative");
+    }
+
+    public void check(String actualResult, String expectedResult, String testName) {
+        if (actualResult.equals(expectedResult)) {
+            System.out.println(testName + " has passed!");
+        } else {
+            System.out.println(testName + " has failed!");
+            System.out.println("Expected " + expectedResult + " but was " + actualResult);
         }
-
-        public void check(String actualResult, String expectedResult, String testName) {
-            if (actualResult.equals(expectedResult)) {
-                System.out.println(testName + " has passed!");
-            } else {
-                System.out.println(testName + " has failed!");
-                System.out.println("Expected " + expectedResult + " but was " + actualResult);
-            }
-        }
+    }
 }
 
